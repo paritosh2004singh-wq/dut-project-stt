fb77366370a33248df39dd9fa6244a000c28ce3a
Paritosh Singh <paritosh2004.singh@gmail.com>
2026-06-30 11:04:36 +0530

    DELETED UNWANTED CONTANTS FILE

--- frontend/src/constants/languages.js
+++
@@ -1,6 +0,0 @@
-export const languageOptions = [
-  { value: "English", label: "English" },
-  { value: "Hindi", label: "Hindi" },
-  { value: "Kannada", label: "Kannada" },
-  { value: "Marathi", label: "Marathi" },
-];
